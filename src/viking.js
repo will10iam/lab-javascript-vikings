9d8d84969f632436846867d66eaafa231b1d56b9
William Berbet <wiiliamb@hotmail.com>
2022-03-28 20:35:34 -0300

    primeiras quatro interações feitas, ultima não consegui terminar

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,76 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor ( health, strength ) { 
+    this.health = health,
+    this.strength = strength
+    this.attack = function() {
+      return this.strength
+    }
+
+    this.receiveDamage = function(Damage) {
+      this.health -= Damage
+     }
+   }
+ }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor (name,health,strength ) {
+    super (health,strength)
+    this.name = name
+    this.receiveDamage = function(Damage) {
+      this.health -= Damage
+      
+    if (this.health > 0) {
+      return `${this.name} has received ${Damage} points of damage`
+    } else {
+      return `${this.name} has died in act of combat`
+    }
+  }
+  this.attack = function() {
+    return this.strength
+  }
+ this.battleCry = function() {
+   return 'Odin Owns You All!';
+ }
+}
+}
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor (name,health,strength ) {
+    super (health,strength)
+
+    this.attack = function() {
+      return this.strength
+    }
+  
+  this.receiveDamage = function(Damage) {
+    this.health -= Damage
+    if (this.health > 0) {
+      return `A Saxon has received ${Damage} points of damage`
+    } else {
+      return 'A Saxon has died in combat'
+      }
+    }
+  } 
+}
 
 // War
-class War {}
+class War {
+  constructor () {
+    this.vikingArmy = []
+    this.saxonArmy = []
+  }
+  addviking(viking) {
+    this.vikingArmy.push(viking)
+  }
+
+  addsaxon(saxon) {
+    this.saxonArmy.push(saxon)
+  }
+}
 
 
 
